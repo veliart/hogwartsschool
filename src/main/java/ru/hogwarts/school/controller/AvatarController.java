d0a0f789683358d5c3243c374858da2de66b217f
Artur Vakhitov <arthurvakhitov@gmail.com>
2024-01-31 16:04:16 +0300

    Add downloadAvatar method in AvatarController

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,15 +1,20 @@
 package ru.hogwarts.school.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.service.AvatarService;
+import ru.hogwarts.school.model.Avatar;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController("student")
 public class AvatarController {
@@ -28,5 +33,29 @@ public class AvatarController {
         }
         avatarService.uploadAvatar(id, avatarFile);
         return ResponseEntity.ok().build();
+    }
+    @GetMapping(value = "/{id}/avatar-from-db")
+    public ResponseEntity<byte[]> downloadAvatar(@PathVariable Long id) {
+        Avatar avatar = avatarService.findStudentAvatar(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+
+    @GetMapping(value = "/{id}/avatar-from-file")
+    public void downloadAvatar(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.findStudentAvatar(id);
+        Path path = Path.of(avatar.getFilePath());
+        try (InputStream inputStream = Files.newInputStream(path);
+             OutputStream outputStream = response.getOutputStream();
+             )
+        {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            inputStream.transferTo(outputStream);
+        }
+
     }
 }
--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,9 +1,12 @@
 package ru.hogwarts.school.service;
 
 import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 
 import java.io.IOException;
 
 public interface AvatarService {
     void uploadAvatar(Long id, MultipartFile avatarFile) throws IOException;
+
+    Avatar findStudentAvatar(Long id);
 }
