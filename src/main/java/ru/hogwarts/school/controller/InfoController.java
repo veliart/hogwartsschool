28091e5aeafcf07c2465f8908dab718d68fb3c91
Artur Vakhitov <arthurvakhitov@gmail.com>
2024-03-03 12:55:37 +0300

    Add 3 method for sum in Info controller

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,12 +1,16 @@
 package ru.hogwarts.school.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Stream;
+
 @RestController
 public class InfoController {
-
+    private final Logger logger = LoggerFactory.getLogger(InfoController.class);
     @Value("${server.port}")
     private Integer port;
 
@@ -14,4 +18,40 @@ public class InfoController {
     public int GetPort() {
         return port;
     }
+    @GetMapping("sum")
+    public long calculateSum() {
+
+        // method 1
+        long startTime = System.currentTimeMillis();
+        long sum = Stream.iterate(1, a -> a +1)
+                .limit(10_000_000)
+                .reduce(0, (a, b) -> a + b );
+        long finishTime = System.currentTimeMillis();
+
+        logger.info("first method time " + (finishTime - startTime));
+
+        // method 2
+        startTime = System.currentTimeMillis();
+        sum = Stream.iterate(1, a -> a +1)
+                .limit(10_000_000)
+                .parallel()
+                .reduce(0, (a, b) -> a + b );
+        finishTime = System.currentTimeMillis();
+
+        logger.info("Second method time" + (finishTime - startTime));
+
+        // method 3
+        startTime = System.currentTimeMillis();
+        sum = 0;
+        for (int i = 0; i <= 10_000_000; i++) {
+            sum += i;
+        }
+        finishTime = System.currentTimeMillis();
+
+        logger.info("Third method time " + (finishTime - startTime));
+
+
+        return sum;
+    }
+
 }
